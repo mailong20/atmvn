46fafa5a0bc269c8db7a4584d565837c46aea10f
Mai Long <63288475+mailong20@users.noreply.github.com>
2023-06-02 09:07:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 
-<<<<<<< Updated upstream:main.py
-from core import auth, floor, home_ui, user, admin_ui
-=======
 from core import auth, floor, home, user, admin_ui, floor_type
->>>>>>> Stashed changes:main2.py
 from database.configuration import engine
 from models import models
 import uvicorn
@@ -33,13 +29,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # app.include_router(floor.router)
 app.include_router(floor_type.router)
 app.include_router(user.router)
-<<<<<<< Updated upstream:main.py
-app.include_router(home_ui.router)
-app.include_router(admin_ui.router)
-=======
 # app.include_router(home.router)
 # app.include_router(admin_ui.router)
->>>>>>> Stashed changes:main2.py
+
 
 
 if __name__ == "__main__":
